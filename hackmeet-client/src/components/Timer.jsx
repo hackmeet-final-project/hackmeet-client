b3f9e2ec5d2d9a7a95396c2c20cdb41694ada293
Shela Amanda Putri <shelaamanda00@gmail.com>
2023-07-28 15:46:24 +0700

    feat: creating timer

--- hackmeet-client/src/components/Timer.jsx
+++ hackmeet-client/src/components/Timer.jsx
@@ -1,30 +1,44 @@
 import React, { useRef } from "react"
 import { useState, useEffect } from 'react'
 
-const Timer = ({seconds}) => {
+
+const Timer = ({seconds, startTimer, onFinish}) => {
   const [timer, setTimer] = useState(seconds)
   const timerId = useRef()
 
+  const formatTime = (time) => {
+    let minutes = Math.floor(time/60)
+    let seconds = Math.floor(time - minutes * 60)
+
+    if(minutes < 10) {
+      minutes = "0" + minutes
+    } 
+    if(seconds < 10) {
+      seconds = "0" + seconds
+    }
+    return minutes + ":" + seconds
+  }
+  
   useEffect(() => {
     timerId.current = setInterval(() => {
       setTimer(prev => prev - 1)
     }, 1000)
 
     return () => clearInterval(timerId.current)
-  }, [])
+  }, [startTimer])
 
   useEffect(() => {
     if(timer <= 0) {
       clearInterval(timerId.current)
       console.log("timer done")
     }
-  }, [timer])
+  }, [timer, onFinish])
 
 
 
   return (
     <div>
-      <h1>Timer: {timer} detik</h1>
+      <h1>Timer: {formatTime(timer)} detik</h1>
     </div>
   )
 }
--- hackmeet-client/src/views/Lobby.jsx
+++ hackmeet-client/src/views/Lobby.jsx
@@ -1,10 +1,50 @@
-import Timer from "../components/Timer"
+import React, { useState, useEffect } from "react";
+import Timer from "../components/Timer";
 
 const Lobby = () => {
+    const [ready, setReady] = useState(false)
+    const [coding, setCoding] = useState(false)
+    const [startTimer, setStartTimer] = useState(false)
+
+    const handleStartMatch = () => {
+        setReady(true)
+    }
+
+    useEffect(() => {
+        if (ready) {
+            setStartTimer(true)
+            setTimeout(() => {
+                setCoding(true)
+            }, 10000)
+        }
+    }, [ready])
+
+    const handleTimerFinish = () => {
+        setStartTimer(false)
+    }
+
+    console.log("ready: " + ready)
+    console.log("coding: " + coding)
+    console.log("timerstart: " + startTimer)
+
     return (
         <>
             <h1>Lobby</h1>
-            <Timer seconds={5} />
+            <button onClick={handleStartMatch}>Start Match</button>
+
+            {ready && !coding && (
+                <>
+                    <h1>Match found, get ready !</h1>
+                    <Timer seconds={10} startTimer={true}/>
+                </>
+            )}
+
+            {coding && (
+                <>
+                    <h1>Gas battle</h1>
+                    <Timer seconds={10} startTimer={startTimer} onFinish={handleTimerFinish} />
+                </>
+            )}
         </>
     )
 }
