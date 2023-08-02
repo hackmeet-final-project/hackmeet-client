d8d44ae48b9ab2f8275725bf683b94bec493a012
marru26 <sabilalm00@gmail.com>
2023-08-02 15:20:50 +0700

    fix: errorhandling for code editor if error and show testcase

--- hackmeet-client/src/components/CodeEditor.jsx
+++ hackmeet-client/src/components/CodeEditor.jsx
@@ -3,30 +3,38 @@ import Swal from "sweetalert2";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useContext } from "react";
+import { useToast } from "@chakra-ui/react";
 import ShakeContext from "../context/ShakeContext";
 
-export default function CodeEditor({getWinner}) {
-  const { animationName, animationCount } = useContext(ShakeContext)
-  const [passed, setPassed] = useState([])
+export default function CodeEditor({ getWinner }) {
+  const toast = useToast();
+  const { animationName, animationCount } = useContext(ShakeContext);
+  const [passed, setPassed] = useState([]);
+  const [showError, setShowError] = useState({
+    argsTest: false,
+    initialCase: [],
+    argsError: false,
+    message: "",
+  });
   const [answer, setAnswer] = useState("");
   const isLoading = useSelector((state) => {
     return state.soal.isLoading;
   });
 
-  const defaultAnswer = useSelector(state => {
-    return state.soal.defaultAnswer
-  })
+  const defaultAnswer = useSelector((state) => {
+    return state.soal.defaultAnswer;
+  });
 
-  const testCases = useSelector(state => {
-    return state.soal.testcases
-  })
+  const testCases = useSelector((state) => {
+    return state.soal.testcases;
+  });
 
   function handleEditorChange(value) {
     setAnswer(value);
   }
 
   function handleSubmit() {
-    setPassed([])
+    setPassed([]);
     const firstParameterIndex = answer.indexOf("(");
     const lastParameterIndex = answer.indexOf(")");
     const firstBracketIndex = answer.indexOf("{");
@@ -43,56 +51,99 @@ export default function CodeEditor({getWinner}) {
 
     testCases.forEach((test) => {
       let result;
-      result = answerFunction(...test.arguments);
+      try {
+        result = answerFunction(...test.arguments);
+        setShowError({ argsError: false });
+      } catch (error) {
+        setShowError({ argsError: true, message: error.message });
+      }
       if (test.answer === result) {
         totalTestPassed++;
-        setPassed(prev => {
-          return prev = [...prev, true]
-        })
+        setPassed((prev) => {
+          return (prev = [...prev, [true, test.arguments, test.answer]]);
+        });
       } else {
-        setPassed(prev => {
-          return prev = [...prev, false]
-        })
+        setPassed((prev) => {
+          return (prev = [...prev, [false, test.arguments, test.answer]]);
+        });
       }
     });
 
     const msg = `Total Test Passed : ${totalTestPassed}\n
     Total Test Failed : ${totalTestFailed}`;
     if (totalTestPassed === totalTest) {
-      Swal.fire("You won!");
-      getWinner()
+      toast({
+        position: "bottom",
+        title: "You Won",
+        description: "See you again!",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
+
+      getWinner();
     } else {
       // Swal.fire("testing...", msg, "question")
-    };
+    }
   }
 
-  const showPassed = passed.map((pass, index) => {
-    if(pass) {
-      return <span key={index + 1}>test_{index + 1} <i className="bi bi-check-square-fill text-success"></i> Passed</span>
+  let showPassed = passed.map((e, index) => {
+    let i = index;
+    if (e[0]) {
+      return (
+        <span key={i + 1}>
+          test_{i + 1} function main[{String(e[1])}] should be {String(e[2])}
+          <i className="bi bi-check-square-fill text-success"></i> Passed
+        </span>
+      );
     } else {
-      return <span key={index + 1}>test_{index + 1} <i className="bi bi-x-square-fill text-danger"></i> Failed</span>
+      return (
+        <span key={i + 1}>
+          test_{i + 1} function main[{String(e[1])}] should be {String(e[2])}{" "}
+          <i className="bi bi-x-square-fill text-danger"></i> Failed
+        </span>
+      );
     }
-  })
+  });
 
   if (isLoading) {
     return <h3 className="m-3">Loading....fetching data</h3>;
   }
-  
+
   return (
-    <div className="w-100 h-100 d-flex flex-column p-1" style={{animation: animationName, animationIterationCount: animationCount}}>
-        <Editor
-          width="100%"
-          height="70%"
-          value={answer}
-          theme="vs-light"
-          defaultLanguage="javascript"
-          defaultValue={defaultAnswer}
-          onChange={handleEditorChange}
-        />
-        <div className="w-100 position-relative px-3 py-2 rounded-bottom-4 d-flex flex-column " style={{height: "30%", boxShadow: "rgba(0, 0, 0, 0.45) 0px -1px 25px -10px"}}>
-          {showPassed}
-          <button className="btn shadow-main text-white button-hover position-absolute" style={{right: 5, bottom: 0, backgroundColor: "var(--third-color)"}} onClick={handleSubmit}>Submit</button>
-        </div>
+    <div
+      className="w-100 h-100 d-flex flex-column p-1"
+      style={{
+        animation: animationName,
+        animationIterationCount: animationCount,
+      }}
+    >
+      <Editor
+        width="100%"
+        height="70%"
+        value={answer}
+        theme="vs-light"
+        defaultLanguage="javascript"
+        defaultValue={defaultAnswer}
+        onChange={handleEditorChange}
+      />
+      <div
+        className="w-100 position-relative px-3 py-2 rounded-bottom-4 d-flex flex-column "
+        style={{
+          height: "30%",
+          boxShadow: "rgba(0, 0, 0, 0.45) 0px -1px 25px -10px",
+        }}
+      >
+        {showError.argsError ? showError.message : ""}
+        {showPassed}
+        <button
+          className="btn shadow-main text-white button-hover position-absolute"
+          style={{ right: 5, bottom: 0, backgroundColor: "var(--third-color)" }}
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
--- hackmeet-client/src/views/Battle.jsx
+++ hackmeet-client/src/views/Battle.jsx
@@ -1,109 +1,193 @@
-import { useState, useRef, useEffect, useContext } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { fetchSoal, generateQuestion } from '../store/actions/question/actionCreator'
-import Timer from '../components/Timer'
-import Chat from '../components/Chat'
-import CodeEditor from '../components/CodeEditor'
-import Media from "../components/Media"
-import MatchFound from "../components/MatchFound"
-import useSound from 'use-sound';
-import popSound from "../audio/popSound.mp3"
-import openSound from "../audio/openSound.mp3"
-import ShakeContext from "../context/ShakeContext"
+import { useState, useRef, useEffect, useContext } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchSoal,
+  generateQuestion,
+} from "../store/actions/question/actionCreator";
+import Timer from "../components/Timer";
+import Chat from "../components/Chat";
+import CodeEditor from "../components/CodeEditor";
+import Media from "../components/Media";
+import MatchFound from "../components/MatchFound";
+import useSound from "use-sound";
+import popSound from "../audio/popSound.mp3";
+import openSound from "../audio/openSound.mp3";
+import ShakeContext from "../context/ShakeContext";
 
 const Battle = () => {
-  const  {animationName, animationCount} = useContext(ShakeContext)
+  const { animationName, animationCount } = useContext(ShakeContext);
   const [play] = useSound(popSound);
   const [play2] = useSound(openSound);
-  const [ready, setReady] = useState(false)
-  const [generateCode, setGenerateCode] = useState(false)
-  const [startCoding, setStartCoding] = useState(false)
-  const [coding, setCoding] = useState(false)
-  const [message, setMessage] = useState('')
-  const [chats, setChats] = useState([])
-  const [hide, setHide] = useState(false)
-  const mediaRef = useRef()
-  const dispatch = useDispatch()
-  const question = useSelector(state => {
-    return state.soal.question
-  })
-    const getDraw = () => {
-      setGenerateCode(false)
-      mediaRef.current.handleGameDraw()
-    }
+  const [ready, setReady] = useState(true); //test state:false
+  const [generateCode, setGenerateCode] = useState(true); //test ediot state:false
+  const [startCoding, setStartCoding] = useState(false);
+  const [coding, setCoding] = useState(false);
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [hide, setHide] = useState(false);
+  const mediaRef = useRef();
+  const dispatch = useDispatch();
+  const question = useSelector((state) => {
+    return state.soal.question;
+  });
+  const getDraw = () => {
+    setGenerateCode(false);
+    mediaRef.current.handleGameDraw();
+  };
 
-    const getWinner = () => {
-      
-      mediaRef.current.handleSetWinner()
-    }
+  const getWinner = () => {
+    mediaRef.current.handleSetWinner();
+  };
 
-    useEffect(() => {
-      if(ready) {
-        dispatch(fetchSoal())
-        .then(soal => {
-          const generateNumber = Math.floor(Math.random() * soal.length)
-          dispatch(generateQuestion(generateNumber))
-          setGenerateCode(true)
-          setHide(true)
-          setTimeout(() => {
-            setStartCoding(true)
-          }, 2000)
-          setTimeout(() => {
-            setHide(false)
-          }, 7000)
+  useEffect(() => {
+    if (ready) {
+      dispatch(fetchSoal())
+        .then((soal) => {
+          const generateNumber = Math.floor(Math.random() * soal.length);
+          dispatch(generateQuestion(generateNumber));
+          setGenerateCode(true);
+          setHide(false); //test editr state:true
+          // setTimeout(() => {
+          //   setStartCoding(true);
+          // }, 2000);
+          // setTimeout(() => {
+          //   setHide(false);
+          // }, 7000);
         })
-        .catch(error => {
-          console.log(error)
-        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-  }, [ready])
+  }, [ready]);
 
-    return (
-      <div className="container-fluid w-100" style={{ height: "100vh", animation: animationName, animationIterationCount: animationCount}}>
-        <MatchFound hide={hide} startCoding={startCoding}/>
-        <div className="container gap-3 py-5 d-flex h-100">
-          <div className="d-flex flex-column gap-3 my-5 w-75 h-100 position-relative pb-4">
-            <Timer coding={coding} setCoding={setCoding} startCoding={startCoding} setStartCoding={setStartCoding} getDraw={getDraw}/>
-            <Media ref={mediaRef} ready={ready} setReady={setReady} message={message} setMessage={setMessage} chats={chats} setChats={setChats} setGenerateCode={setGenerateCode} setCoding={setCoding}/>
-            <div className='d-flex' style={{height: '50%'}}>
-              <div className="h-100 w-50 shadow-main d-flex flex-column overflow-hidden rounded-start-4 bg-white" style={{border: '3px solid white', boxShadow: "rgb(204, 219, 232) 3px 3px 6px 0px inset, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px inset"}}>
-                  {generateCode ? <CodeEditor getWinner={getWinner}/> : ''}
-              </div>
-              <div className="h-100 w-50 shadow-main d-flex align-items-center justify-content-center overflow-hidden rounded-end-4 position-relative px-5" style={{border: '3px solid white', background: 'var(--secondary-color)'}}>
-                  {generateCode ? 
-                    <p className='text-dark fs-5' style={{textAlign: 'justify'}}>
-                      {question}
-                    </p> 
-                    : 
-                    <div className="position-relative d-flex align-items-center justify-content-center">
-                      <i className="bi bi-question position-absolute display-1"></i>
-                      <i className="bi bi-cloud-fill text-white" style={{fontSize: '300px', textShadow: "3px 4px 10px rgba(0, 0, 0, 0.57)"}}>
-                      </i>
-                    </div> 
-                  }
-                <div className="d-flex position-absolute gap-1" style={{width: "230px", bottom: 10, right: 5}}>
-                  <button className="btn w-50 shadow-main text-dark button-hover fw-bold" style={{backgroundColor: "var(--primary-color)"}} onClick={() => mediaRef.current.handleFindMatch()}>Find Match</button>
-                  <button className="btn w-50 shadow-main text-dark button-hover fw-bold" style={{backgroundColor: "var(--fourth-color)"}} onClick={() => mediaRef.current.handleLeaveMatch()}>Leave</button>
+  return (
+    <div
+      className="container-fluid w-100"
+      style={{
+        height: "100vh",
+        animation: animationName,
+        animationIterationCount: animationCount,
+      }}
+    >
+      <MatchFound hide={hide} startCoding={startCoding} />
+      <div className="container gap-3 py-5 d-flex h-100">
+        <div className="d-flex flex-column gap-3 my-5 w-75 h-100 position-relative pb-4">
+          <Timer
+            coding={coding}
+            setCoding={setCoding}
+            startCoding={startCoding}
+            setStartCoding={setStartCoding}
+            getDraw={getDraw}
+          />
+          <Media
+            ref={mediaRef}
+            ready={ready}
+            setReady={setReady}
+            message={message}
+            setMessage={setMessage}
+            chats={chats}
+            setChats={setChats}
+            setGenerateCode={setGenerateCode}
+            setCoding={setCoding}
+          />
+          <div className="d-flex" style={{ height: "50%" }}>
+            <div
+              className="h-100 w-50 shadow-main d-flex flex-column overflow-hidden rounded-start-4 bg-white"
+              style={{
+                border: "3px solid white",
+                boxShadow:
+                  "rgb(204, 219, 232) 3px 3px 6px 0px inset, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px inset",
+              }}
+            >
+              {generateCode ? <CodeEditor getWinner={getWinner} /> : ""}
+            </div>
+            <div
+              className="h-100 w-50 shadow-main d-flex align-items-center justify-content-center overflow-hidden rounded-end-4 position-relative px-5"
+              style={{
+                border: "3px solid white",
+                background: "var(--secondary-color)",
+              }}
+            >
+              {generateCode ? (
+                <p className="text-dark fs-5" style={{ textAlign: "justify" }}>
+                  {question}
+                </p>
+              ) : (
+                <div className="position-relative d-flex align-items-center justify-content-center">
+                  <i className="bi bi-question position-absolute display-1"></i>
+                  <i
+                    className="bi bi-cloud-fill text-white"
+                    style={{
+                      fontSize: "300px",
+                      textShadow: "3px 4px 10px rgba(0, 0, 0, 0.57)",
+                    }}
+                  ></i>
                 </div>
-              <div className="d-flex position-absolute gap-1" style={{ width: "230px", bottom: 10, right: 5 }}>
-                <button className="btn w-50 shadow-main text-dark button-hover fw-bold" style={{ backgroundColor: "var(--primary-color)" }} onClick={() => {
-                  mediaRef.current.handleFindMatch();
-                  play2()
-                  }}>Find Match</button>
-                <button className="btn w-50 shadow-main text-dark button-hover fw-bold" style={{ backgroundColor: "var(--fourth-color)" }} onClick={() => {
-                  mediaRef.current.handleLeaveMatch();
-                  play()
-                }}>Leave</button>
+              )}
+              <div
+                className="d-flex position-absolute gap-1"
+                style={{ width: "230px", bottom: 10, right: 5 }}
+              >
+                <button
+                  className="btn w-50 shadow-main text-dark button-hover fw-bold"
+                  style={{ backgroundColor: "var(--primary-color)" }}
+                  onClick={() => mediaRef.current.handleFindMatch()}
+                >
+                  Find Match
+                </button>
+                <button
+                  className="btn w-50 shadow-main text-dark button-hover fw-bold"
+                  style={{ backgroundColor: "var(--fourth-color)" }}
+                  onClick={() => mediaRef.current.handleLeaveMatch()}
+                >
+                  Leave
+                </button>
+              </div>
+              <div
+                className="d-flex position-absolute gap-1"
+                style={{ width: "230px", bottom: 10, right: 5 }}
+              >
+                <button
+                  className="btn w-50 shadow-main text-dark button-hover fw-bold"
+                  style={{ backgroundColor: "var(--primary-color)" }}
+                  onClick={() => {
+                    mediaRef.current.handleFindMatch();
+                    play2();
+                  }}
+                >
+                  Find Match
+                </button>
+                <button
+                  className="btn w-50 shadow-main text-dark button-hover fw-bold"
+                  style={{ backgroundColor: "var(--fourth-color)" }}
+                  onClick={() => {
+                    mediaRef.current.handleLeaveMatch();
+                    play();
+                  }}
+                >
+                  Leave
+                </button>
               </div>
             </div>
           </div>
         </div>
-        <div className="w-25 shadow-main rounded-4 my-5 d-flex align-items-center justify-content-center p-2 position-relative" style={{ border: '3px solid white', backgroundColor: "rgba(255, 255, 255, 0.3)" }}>
-          <Chat setMessage={setMessage} message={message} chats={chats} sendMessage={(event) => mediaRef.current.sendMessage(event)} />
+        <div
+          className="w-25 shadow-main rounded-4 my-5 d-flex align-items-center justify-content-center p-2 position-relative"
+          style={{
+            border: "3px solid white",
+            backgroundColor: "rgba(255, 255, 255, 0.3)",
+          }}
+        >
+          <Chat
+            setMessage={setMessage}
+            message={message}
+            chats={chats}
+            sendMessage={(event) => mediaRef.current.sendMessage(event)}
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Battle
+export default Battle;
